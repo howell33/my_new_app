988fadfa010537096a8d0251764dfdb0d8286142
Alicia Howell <howell33@msu.edu>
2015-03-30 18:22:05 -0400

    Model App

--- my_new_app/app/helpers/static_helper.rb
+++
@@ -1,5 +0,0 @@
-module StaticHelper
-	def current_date_time
-		Time.now.strftime('%B %-d, %Y at %-1:%M%P')
-	end
-end
--- my_new_app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-MyNewApp::Application.config.secret_token = '53ec5e7e0ad6ce2818b04c3f7743e6475694bb709fc14632157db3a041dca1926395a5ef4c1495c27a0ce01a9a93be30ff01dd5658ed9be6d10c9b09519a9b75'
